1b24f1ae05cc28f8421d11224708537c41894416
Kelvin Mwangi <kelvin.mwangi@squaddigital.com>
2019-06-19 16:00:47 +0300

    resolving javascript error

--- wp-content/themes/optimus-mini/footer.php
+++ wp-content/themes/optimus-mini/footer.php
@@ -1,6 +1,27 @@
 
-<script type="text/javascript" src="<?php echo get_template_directory_uri() . '/js/main.js'; ?>">
-</div>
-</div>
+<footer class="footer">
+    <div class="inner-wrapper">
+        <h3>CONTACT US</h3>
+        <ul>
+            <li><span>Phone:</span> <a href="tel:0700000000"> 0700 000 000</a></li>
+            <li><span>Email:</span> <a href="mailto:info@squad.com">info@squad.com</a></li>
+            <li><span>Address:</span> 5th Floor, The Chancery <br>Valley Road</li>
+            <li>P. O. Box 0000 GPO 00100 <br>Nairobi ,Kenya</li>
+        </ul>
+
+        <div class="copyright">
+
+            <ul>
+                <li>© 2019 Squad</li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Terms of Use</a></li>
+
+            </ul>
+        </div>
+
+    </div>
+</footer>
+
+<script type="text/javascript" src="<?php echo get_template_directory_uri() . '/js/main.js'; ?>"></script>
 </body>
 </html>
