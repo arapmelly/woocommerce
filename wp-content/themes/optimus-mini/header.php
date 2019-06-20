03512e67becc7f7c0e64e8f8dae885cd7bf9834c
Kelvin Mwangi <kelvin.mwangi@squaddigital.com>
2019-06-20 10:39:29 +0300

    getting latest

--- wp-content/themes/optimus-mini/header.php
+++ wp-content/themes/optimus-mini/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Hello World</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="<?php  bloginfo('stylesheet_url'); ?>" rel="stylesheet">
+    <link href="<?php bloginfo('stylesheet_url'); ?>" rel="stylesheet">
 
 </head>
 
@@ -15,12 +15,16 @@
     <div class="sections">
         <div class="left">
             <a href="#" title="menu toggle" class="menu actionButton sidebarTrigger"><span
-                    class="icon-menu-outline"/></a>
-            <a href="<?php echo get_site_url(); ?>" title="shop logo" class="logo"><img src="<?php  echo get_template_directory_uri(); ?>/images/logo-copy.png"
-                                                            srcset="<?php  echo get_template_directory_uri(); ?>/images/logo-copy@2x.png 2x,<?php  echo get_template_directory_uri(); ?>/images/logo-copy@3x.png 3x"></a>
+                        class="icon-menu-outline"></span></a>
+            <a href="<?php echo get_site_url(); ?>" title="shop logo" class="logo"><img
+                        src="<?php echo get_template_directory_uri(); ?>/images/logo-copy.png"
+                        srcset="<?php echo get_template_directory_uri(); ?>/images/logo-copy@2x.png 2x,
+                        <?php echo get_template_directory_uri(); ?>/images/logo-copy@3x.png 3x"></a>
         </div>
+
         <div class="right">
-            <a href="<?php wc_get_cart_url(); ?>" title="shopping cart" class="shopping-cart"><span class="icon-shopping-cart-outline"/></a>
+            <a href="<?php wc_get_cart_url(); ?>" title="shopping cart" class="shopping-cart"><span
+                        class="icon-shopping-cart-outline"></span></a>
         </div>
     </div>
 </nav>
