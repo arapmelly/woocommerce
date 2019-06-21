dfe880395f0a70d37e3fd23b958c6f87533f9cee
Kelvin Mwangi <kelvin.mwangi@squaddigital.com>
2019-06-21 15:44:14 +0300

    styling home page

--- wp-content/themes/optimus-mini/header.php
+++ wp-content/themes/optimus-mini/header.php
@@ -16,12 +16,12 @@
     <div class="sections">
         <div class="left">
             <a href="#" title="menu toggle" class="menu actionButton sidebarTrigger"><span
-                    class="icon-menu-outline"/></a>
+                        class="icon-menu-outline"></span></a>
             <a href="<?php echo get_site_url(); ?>" title="shop logo" class="logo"><img src="<?php  echo get_template_directory_uri(); ?>/images/logo-copy.png"
                                                             srcset="<?php  echo get_template_directory_uri(); ?>/images/logo-copy@2x.png 2x,<?php  echo get_template_directory_uri(); ?>/images/logo-copy@3x.png 3x"></a>
         </div>
         <div class="right">
-            <a href="<?php echo ('cart'); ?>" title="shopping cart" class="shopping-cart"><span class="icon-shopping-cart-outline"/></a>
+            <a href="<?php echo ('cart'); ?>" title="shopping cart" class="shopping-cart"><span class="icon-shopping-cart-outline"></span></a>
         </div>
     </div>
 </nav>
--- wp-content/themes/optimus-mini/woocommerce.php
+++ wp-content/themes/optimus-mini/woocommerce.php
@@ -1,151 +1,146 @@
-<?php
-get_header();
+<?php get_header(); ?>
 
+<?php if ( is_front_page() ) { ?>
+    <section class="shop-banner margin-top-phone-7">
+		<?php echo do_shortcode( '[shop-banner]' ); ?>
+    </section>
+<?php } ?>
 
-?>
-    <?php if(is_front_page()) { ?> 
-	   <section class="shop-banner margin-top-phone-7">
-    	   <?php echo do_shortcode('[shop-banner]'); ?>
-	   </section>
-    <?php } ?>
-
-<?php if(is_front_page()) { ?> 
-<main class="main-content">
-	<section class="business-details">
+<?php if ( is_front_page() ) { ?>
+    <main class="main-content">
+    <section class="business-details">
         <div class="section-inner-wrapper">
-            <h1 class="business-name"><?php echo bloginfo('sitename'); ?></h1>
+            <h1 class="business-name"><?php echo bloginfo( 'sitename' ); ?></h1>
             <div class="rating-widget">
                 <div class="stars" data-score="4.5"></div>
                 <div class="num-rating tag-descriptor">4.25 <span class="total-reviews">(560 Reviews)</span></div>
             </div>
             <div class="orders-made">
-                <div class="tag-descriptor"><?php echo get_total_orders('complete'); ?> <span class="total-reviews">Orders made</span></div>
-                <div class="tag-descriptor"><?php echo get_total_orders('on-hold'); ?> <span class="total-reviews">Deliveries made</span></div>
+                <div class="tag-descriptor"><?php echo get_total_orders( 'complete' ); ?> <span class="total-reviews">Orders made</span>
+                </div>
+                <div class="tag-descriptor"><?php echo get_total_orders( 'on-hold' ); ?> <span class="total-reviews">Deliveries made</span>
+                </div>
             </div>
         </div>
     </section>
 
 <?php } ?>
 
-    
 
+<?php
 
-    <?php 
+	$args = array(
+		'type'         => 'product',
+		'parent'       => get_queried_object_id(),
+		'orderby'      => 'term_group',
+		'hide_empty'   => true,
+		'hierarchical' => 1,
+		'taxonomy'     => 'product_cat',
+		'pad_counts'   => false
+	);
 
-$args = array(
-    'type'                     => 'product',
-    'parent'                   => get_queried_object_id(),
-    'orderby'                  => 'term_group',
-    'hide_empty'               => true,
-    'hierarchical'             => 1,
-    'taxonomy'                 => 'product_cat',
-    'pad_counts'               => false
-);
-
-$cats = get_categories( $args );
+	$cats = get_categories( $args );
 
 ?>
 
     <section class="filter-by-category">
         <div class="section-inner-wrapper">
             <div class="owl-carousel owl-theme">
-                <?php foreach($cats as $cat) { ?>
+				<?php foreach ( $cats as $cat ) { ?>
                     <div class="item">
-                        <a href="<?php echo  get_term_link( $cat->term_taxonomy_id, 'product_cat' ); ?>"><h4><?php echo $cat->name; ?></h4></a>
+                        <a href="<?php echo get_term_link( $cat->term_taxonomy_id, 'product_cat' ); ?>">
+                            <h4><?php echo $cat->name; ?></h4></a>
                     </div>
-                <?php } ?>
+				<?php } ?>
             </div>
             <div class="custom-nav owl-nav"></div>
         </div>
     </section>
 
 
-
     <section class="products-by-category">
-        <?php foreach($cats as $cat) { ?>
-        <div class="products">
-            <header class="category-header">
-                <div class="inner-wrapper">
-                    <h3><?php echo $cat->name; ?> </h3>
-                    <a href="<?php echo  get_term_link( $cat->term_taxonomy_id, 'product_cat' ); ?>">View All</a>
-                </div>
-            </header>
+		<?php foreach ( $cats as $cat ) { ?>
+            <div class="products">
+                <header class="category-header">
+                    <div class="inner-wrapper">
+                        <h3><?php echo $cat->name; ?> </h3>
+                        <a href="<?php echo get_term_link( $cat->term_taxonomy_id, 'product_cat' ); ?>">View All</a>
+                    </div>
+                </header>
 
-            <div class="section-inner-wrapper">
-                <div class="products-slider">
-                    <div class="owl-carousel owl-theme ui link cards">
+                <div class="section-inner-wrapper">
+                    <div class="products-slider">
+                        <div class="owl-carousel owl-theme ui link cards">
 
-                    <?php 
-$query = new WC_Product_Query( array(
-    'limit' => 5,
-    'orderby' => 'date',
-    'order' => 'DESC',
-    'category' => [$cat->slug],
-    
-) );
-$products = $query->get_products();
+							<?php $query    = new WC_Product_Query( array(
+									'limit'    => 5,
+									'orderby'  => 'date',
+									'order'    => 'DESC',
+									'category' => [ $cat->slug ],
 
-foreach($products as $product){
-    
-    ?>
+								) );
 
-                        <div class="item card">
-                            <div class="image">
-                                
-                                <span class="caol-ila_1984"><?php echo $product->get_image(); ?></span>
-                            </div>
-                            <div class="content price-discount">
-                                    <div class="header price"><?php echo wc_price($product->get_price()); ?></div>
-                                    
-                                    <?php if($product->is_on_sale()) { ?> 
-                                        <div class="header price" style="text-decoration:line-through"><?php echo wc_price($product->get_regular_price()); ?></div>
-                                        <div class="discount"><?php echo get_percentage_discount($product); ?> Off</div>
-                                    <?php } ?>
-                                    
-                            </div>
-                            <div class="content">
-                                <div class="header"><?php echo $product->get_name(); ?></div>
-                                <div class="description">
-                                    <?php echo $product->get_description(); ?>
-                                </div>
-                            </div>
+								$products = $query->get_products();
 
-                            <a href="<?php echo get_permalink( $product->get_id() ); ?>" data-quantity="1" class="ui bottom attached button">
-                                buy
-                            </a>
-                        </div>
+								foreach ( $products as $product ) { ?>
 
-<?php } ?>
+                                    <div class="item card">
+                                        <div class="image"><?php echo $product->get_image(); ?></div>
+                                        <div class="content price-discount">
+                                            <div class="header price">
+                                                <h2 class="current-price"><?php echo wc_price( $product->get_price() ); ?></h2>
 
-                    </div> <!-- end of owl-carousel -->
-                </div> <!-- end of products slider -->
-            </div> <!-- end of inner wrapper -->
+	                                            <?php if ( $product->is_on_sale() ) { ?>
+                                                    <h2 class="previous-price"><?php echo wc_price( $product->get_regular_price() ); ?></h2>
+	                                            <?php } ?>
+
+                                                <div class="discount"><?php echo get_percentage_discount( $product ); ?> Off </div>
+
+                                            </div>
+                                        </div>
+
+                                        <div class="content">
+                                            <div class="header"><?php echo $product->get_name(); ?></div>
+<!--                                            <div class="description">-->
+<!--												--><?php //echo $product->get_description(); ?>
+
+<!--	                                            --><?php //echo wp_trim_words($product->get_description(), 15, ' ...'); ?>
+<!--                                            </div>-->
+                                        </div>
+
+                                        <a href="<?php echo get_permalink( $product->get_id() ); ?>" class="ui bottom attached button">buy</a>
+
+                                    </div>
+
+								<?php } ?>
+
+                        </div> <!-- end of owl-carousel -->
+                    </div> <!-- end of products slider -->
+                </div> <!-- end of inner wrapper -->
 
 
-        </div>    <!-- end of products -->   
-        <?php } ?>
-    
+            </div>    <!-- end of products -->
+		<?php } ?>
+
     </section>
 
 
+<?php
 
-    <?php 
+	if ( ! is_front_page() ) {
+		woocommerce_content();
+	}
 
-    if(!is_front_page()){
-        woocommerce_content();
-    }
+?>
 
-    ?>
-    
 
-</main>
+    </main>
 
 
 
 <?php
 
 
-
-get_footer();
+	get_footer();
 
 ?>
