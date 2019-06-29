a2eff3655f8deafba8361497855b449fd7d022a5
Kelvin Mwangi <kelvin.mwangi@squaddigital.com>
2019-06-29 11:47:43 +0300

    getting latest

--- wp-content/themes/optimus-mini/index.php
+++ wp-content/themes/optimus-mini/index.php
@@ -1,48 +1,37 @@
 <?php
-/**
- * Theme Index Section for our theme.
- *
- * @package ThemeGrill
- * @subpackage Spacious
- * @since Spacious 1.0
- */
-get_header();?>
+	/**
+	 * Theme Index Section for our theme.
+	 *
+	 * @package ThemeGrill
+	 * @subpackage Spacious
+	 * @since Spacious 1.0
+	 */
+	get_header(); ?>
 
 
+<?php if ( have_posts() ): ?>
+
+	<?php while ( have_posts() ): the_post(); ?>
 
 
+		<?php
 
-			<?php if (have_posts()): ?>
+		if ( is_front_page() ) {
+			get_template_part( 'content', get_post_format() );
 
-				<?php while (have_posts()): the_post();?>
-
-
-																								<?php
-
-	if (is_front_page()) {
-		get_template_part('content', get_post_format());
-
-	} else {
-		the_content();
-	}
-	?>
+		} else {
+			the_content();
+		}
+		?>
 
 
+	<?php endwhile; ?>
+
+<?php else: ?>
+
+	<?php echo '<p>There are no products at the moment</p>'; ?>
+
+<?php endif; ?>
 
 
-
-																							<?php endwhile;?>
-
-			<?php else: ?>
-
-				<?php echo '<p>There are no products at the moment</p>'; ?>
-
-			<?php endif;?>
-
-
-
-
-
-
-
-<?php get_footer();?>
+<?php get_footer(); ?>
