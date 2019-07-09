974588eec538fd60aede726d17fd0a1d52b12c16
Jacob Chumo <002028@scangroup.local>
2019-07-09 14:15:34 +0300

    new updates

--- wp-content/themes/optimus-mini/woocommerce/cart/cart-totals.php
+++ wp-content/themes/optimus-mini/woocommerce/cart/cart-totals.php
@@ -1,105 +1,106 @@
 <?php
-	/**
-	 * Cart totals
-	 *
-	 * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-totals.php.
-	 *
-	 * HOWEVER, on occasion WooCommerce will need to update template files and you
-	 * (the theme developer) will need to copy the new files to your theme to
-	 * maintain compatibility. We try to do this as little as possible, but it does
-	 * happen. When this occurs the version of the template file will be bumped and
-	 * the readme will list any important changes.
-	 *
-	 * @see        https://docs.woocommerce.com/document/template-structure/
-	 * @package    WooCommerce/Templates
-	 * @version     2.3.6
-	 */
+/**
+ * Cart totals
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/cart/cart-totals.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see        https://docs.woocommerce.com/document/template-structure/
+ * @package    WooCommerce/Templates
+ * @version     2.3.6
+ */
 
-	if ( ! defined( 'ABSPATH' ) ) {
-		exit;
-	}
+if (!defined('ABSPATH')) {
+	exit;
+}
 
 ?>
 
-<div class="cart_totals <?php echo ( WC()->customer->has_calculated_shipping() ) ? 'calculated_shipping' : ''; ?>">
+<div class="cart_totals <?php echo (WC()->customer->has_calculated_shipping()) ? 'calculated_shipping' : ''; ?>">
 
     <div class="cart_totals-inner">
         <table class="ui celled table">
             <thead>
                 <tr>
                     <th colspan="4">
-                        <h2><?php _e( 'Cart Total', 'woocommerce' ); ?></h2>
+                        <h2><?php _e('Cart Total', 'woocommerce');?></h2>
                     </th>
+
                 </tr>
             </thead>
             <tbody>
             <tr>
-                <td><?php _e( 'Subtotal', 'woocommerce' ); ?></td>
-                <td data-title="<?php esc_attr_e( 'Subtotal', 'woocommerce' ); ?>"><?php wc_cart_totals_subtotal_html(); ?></td>
+                <td><?php _e('Subtotal', 'woocommerce');?></td>
+                <td data-title="<?php esc_attr_e('Subtotal', 'woocommerce');?>"><?php wc_cart_totals_subtotal_html();?></td>
             </tr>
 
-            <?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
-                <tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-                    <td><?php wc_cart_totals_coupon_label( $coupon ); ?></td>
-                    <td data-title="<?php echo esc_attr( wc_cart_totals_coupon_label( $coupon, false ) ); ?>"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+            <?php foreach (WC()->cart->get_coupons() as $code => $coupon): ?>
+                <tr class="cart-discount coupon-<?php echo esc_attr(sanitize_title($code)); ?>">
+                    <td><?php wc_cart_totals_coupon_label($coupon);?></td>
+                    <td data-title="<?php echo esc_attr(wc_cart_totals_coupon_label($coupon, false)); ?>"><?php wc_cart_totals_coupon_html($coupon);?></td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endforeach;?>
 
-            <?php foreach ( WC()->cart->get_coupons() as $code => $coupon ) : ?>
-                <tr class="cart-discount coupon-<?php echo esc_attr( sanitize_title( $code ) ); ?>">
-                    <td><?php wc_cart_totals_coupon_label( $coupon ); ?></td>
-                    <td data-title="<?php echo esc_attr( wc_cart_totals_coupon_label( $coupon, false ) ); ?>"><?php wc_cart_totals_coupon_html( $coupon ); ?></td>
+            <?php foreach (WC()->cart->get_coupons() as $code => $coupon): ?>
+                <tr class="cart-discount coupon-<?php echo esc_attr(sanitize_title($code)); ?>">
+                    <td><?php wc_cart_totals_coupon_label($coupon);?></td>
+                    <td data-title="<?php echo esc_attr(wc_cart_totals_coupon_label($coupon, false)); ?>"><?php wc_cart_totals_coupon_html($coupon);?></td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endforeach;?>
 
-            <?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+            <?php if (WC()->cart->needs_shipping() && WC()->cart->show_shipping()): ?>
 
-	            <?php do_action( 'woocommerce_cart_totals_before_shipping' ); ?>
+	            <?php do_action('woocommerce_cart_totals_before_shipping');?>
 
-	            <?php wc_cart_totals_shipping_html(); ?>
+	            <?php wc_cart_totals_shipping_html();?>
 
-	            <?php do_action( 'woocommerce_cart_totals_after_shipping' ); ?>
+	            <?php do_action('woocommerce_cart_totals_after_shipping');?>
 
-            <?php elseif ( WC()->cart->needs_shipping() && 'yes' === get_option( 'woocommerce_enable_shipping_calc' ) ) : ?>
+            <?php elseif (WC()->cart->needs_shipping() && 'yes' === get_option('woocommerce_enable_shipping_calc')): ?>
 
                 <tr class="shipping">
-                    <td><?php _e( 'Shipping', 'woocommerce' ); ?></td>
-                    <td data-title="<?php esc_attr_e( 'Shipping', 'woocommerce' ); ?>"><?php woocommerce_shipping_calculator(); ?></td>
+                    <td><?php _e('Shipping', 'woocommerce');?></td>
+                    <td data-title="<?php esc_attr_e('Shipping', 'woocommerce');?>"><?php woocommerce_shipping_calculator();?></td>
                 </tr>
 
-            <?php endif; ?>
+            <?php endif;?>
 
-            <?php foreach ( WC()->cart->get_fees() as $fee ) : ?>
+            <?php foreach (WC()->cart->get_fees() as $fee): ?>
                 <tr class="fee">
-                    <th><?php echo esc_html( $fee->name ); ?></th>
-                    <td data-title="<?php echo esc_attr( $fee->name ); ?>"><?php wc_cart_totals_fee_html( $fee ); ?></td>
+                    <th><?php echo esc_html($fee->name); ?></th>
+                    <td data-title="<?php echo esc_attr($fee->name); ?>"><?php wc_cart_totals_fee_html($fee);?></td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endforeach;?>
 
-            <?php if ( wc_tax_enabled() && ! WC()->cart->display_prices_including_tax() ) :
-	            $taxable_address = WC()->customer->get_taxable_address();
-	            $estimated_text = WC()->customer->is_customer_outside_base() && ! WC()->customer->has_calculated_shipping()
-		            ? sprintf( ' <small>' . __( '(estimated for %s)', 'woocommerce' ) . '</small>', WC()->countries->estimated_for_prefix( $taxable_address[0] ) . WC()->countries->countries[ $taxable_address[0] ] )
-		            : '';
+            <?php if (wc_tax_enabled() && !WC()->cart->display_prices_including_tax()):
+	$taxable_address = WC()->customer->get_taxable_address();
+	$estimated_text = WC()->customer->is_customer_outside_base() && !WC()->customer->has_calculated_shipping()
+	? sprintf(' <small>' . __('(estimated for %s)', 'woocommerce') . '</small>', WC()->countries->estimated_for_prefix($taxable_address[0]) . WC()->countries->countries[$taxable_address[0]])
+	: '';
 
-	            if ( 'itemized' === get_option( 'woocommerce_tax_total_display' ) ) : ?>
-		            <?php foreach ( WC()->cart->get_tax_totals() as $code => $tax ) : ?>
-                        <tr class="tax-rate tax-rate-<?php echo sanitize_title( $code ); ?>">
-                            <td><?php echo esc_html( $tax->label ) . $estimated_text; ?></td>
-                            <td data-title="<?php echo esc_attr( $tax->label ); ?>"><?php echo wp_kses_post( $tax->formatted_amount ); ?></td>
-                        </tr>
-		            <?php endforeach; ?>
-	            <?php else : ?>
+	if ('itemized' === get_option('woocommerce_tax_total_display')): ?>
+						            <?php foreach (WC()->cart->get_tax_totals() as $code => $tax): ?>
+				                        <tr class="tax-rate tax-rate-<?php echo sanitize_title($code); ?>">
+				                            <td><?php echo esc_html($tax->label) . $estimated_text; ?></td>
+				                            <td data-title="<?php echo esc_attr($tax->label); ?>"><?php echo wp_kses_post($tax->formatted_amount); ?></td>
+				                        </tr>
+						            <?php endforeach;?>
+	            <?php else: ?>
                     <tr class="tax-total">
-                        <td><?php echo esc_html( WC()->countries->tax_or_vat() ) . $estimated_text; ?></td>
-                        <td data-title="<?php echo esc_attr( WC()->countries->tax_or_vat() ); ?>"><?php wc_cart_totals_taxes_total_html(); ?></td>
+                        <td><?php echo esc_html(WC()->countries->tax_or_vat()) . $estimated_text; ?></td>
+                        <td data-title="<?php echo esc_attr(WC()->countries->tax_or_vat()); ?>"><?php wc_cart_totals_taxes_total_html();?></td>
                     </tr>
-	            <?php endif; ?>
-            <?php endif; ?>
+	            <?php endif;?>
+            <?php endif;?>
 
             <tr class="order-total">
-                <td><?php _e( 'Total', 'woocommerce' ); ?></td>
-                <td data-title="<?php esc_attr_e( 'Total', 'woocommerce' ); ?>"><?php wc_cart_totals_order_total_html(); ?></td>
+                <td><?php _e('Total', 'woocommerce');?></td>
+                <td data-title="<?php esc_attr_e('Total', 'woocommerce');?>"><?php wc_cart_totals_order_total_html();?></td>
             </tr>
 
             </tbody>
@@ -108,7 +109,7 @@
                 <tr>
                     <th colspan="4">
                         <div class="ui right floated small primary labeled icon button">
-                            <?php do_action( 'woocommerce_proceed_to_checkout' ); ?>
+                            <?php do_action('woocommerce_proceed_to_checkout');?>
                         </div>
                     </th>
                 </tr>
--- wp-content/themes/optimus-mini/woocommerce/single-product/title.php
+++ wp-content/themes/optimus-mini/woocommerce/single-product/title.php
@@ -30,17 +30,20 @@ $average = $product->get_average_rating();
 <section class="product-details">
     <div class="section-inner-wrapper">
         <h1 class="product-name"><?php echo $product->get_name(); ?></h1>
-
         <div class="price">
-        	<?php if ($product->get_sale_price() <= 0) {?>
-        		<h2 class="current-price"><?php echo wc_price($product->get_regular_price()); ?></h2>
-        	<?php }?>
-
-			<?php if ($product->get_sale_price() > 0) {?>
-                <h2 class="previous-price"><?php echo wc_price($product->get_regular_price()); ?></h2>
-			<?php }?>
-
+            <h2 class="current-price"><?php echo $product->get_price_html(); ?></h2>
         </div>
+
+        <!-- <div class="price">
+        	<?php //if ($product->get_sale_price() <= 0) {?>
+        		<h2 class="current-price" id="productPrice"><?php //echo wc_price($product->get_price_html()); ?></h2>
+        	<?php //}?>
+
+			<?php //if ($product->get_sale_price() > 0) {?>
+                <h2 class="previous-price" id="productPrice"><?php //echo wc_price($product->get_price_html()); ?></h2>
+			<?php //}?>
+
+        </div> -->
 
 		<?php if ($average >= 3) {?>
 
@@ -59,68 +62,136 @@ $average = $product->get_average_rating();
 
 		<?php }?>
 
+        <?php
+//create_product_var($product);
 
+?>
 
-		<?php do_action('woocommerce_before_add_to_cart_button');?>
-
-
-		<?php $attributes = get_prod_attributes($product);?>
-
-		<?php if (count($attributes) > 0) {
-	?>
-
-			<?php
-
-	foreach ($attributes as $key => $attribute) {?>
-
-                <label><?php echo $key; ?></label>
-                <select>
-					<?php $values = explode(',', $attribute);?>
-					<?php foreach ($values as $value) {?>
-                        <option value="<?php echo $value; ?>"><?php echo $value; ?></option>
-					<?php }?>
-
-                </select>
-
-
-			<?php }?>
-
-
-		<?php }?>
+		<?php //do_action('woocommerce_before_add_to_cart_button');?>
 
 
 
-		<?php $variations = json_decode(get_product_variations($product));?>
-
-		<?php if (count($variations) > 0) {
-	?>
-
-
-
-
-
-                <label>Variation</label>
-                <select>
-					<?php foreach ($variations as $variation) {?>
-
-                        <option value="<?php echo $variation->name; ?>"><?php echo $variation->name; ?></option>
-					<?php }?>
-
-                </select>
-
-
-
-
-
-		<?php }?>
 
         <div class="actions">
 
-            <a href="<?php echo do_shortcode('[add_to_cart_url id=<?php echo $product->get_id(); ?>]'); ?>"
-               class="ui button">buy</a>
+           <!--  <form method="post" action="<?php //echo get_template_directory_uri() . '/post_order.php'; ?>" >
+
+                <input type="hidden" id="productId" value="<?php //echo $product->get_id(); ?>" name="productId">
+
+                <input type="hidden" id="atrribute"  value="" name="attribute">
+
+                <input type="hidden" id="variation" value="" name="variation">
+
+                <input type="hidden"  id="variationPrice" value="" name="variationPrice">
+
+
+                <input type="submit" class="ui button" value="Buy">
+
+
+
+            </form> -->
+
+
+            <!--  <a href="<?php //echo do_shortcode('[add_to_cart_url id=<?php echo $product->get_id(); ?>]'); ?>"
+               class="ui button">buy</a> -->
         </div>
 
 		<?php do_action('woocommerce_after_add_to_cart_button');?>
 
     </div>
 </section>
+
+
+<script type="text/javascript">
+
+    function setVariationPrice(){
+
+        var variation = document.getElementById("variationSelect").options[document.getElementById("variationSelect").selectedIndex].value;
+        console.log(variation);
+
+        var vars = variation.split("-");
+
+        var is_discounted = vars[2];
+
+        var product_unit_price = vars[1];
+
+        if(is_discounted > 0){
+            var start_date = vars[4];
+            var end_date = vars[5];
+
+            var current_date = getTodayDate();
+            //check if todays date is within the discount period
+            var datecheck = dateCheck(start_date, end_date, current_date);
+
+            if(dateCheck){
+                product_unit_price = vars[3];
+            }
+
+        }
+
+        console.log(vars);
+
+        var price = formatPrice(product_unit_price, 'KSH');
+
+        document.getElementById("productPrice").innerHTML = price;
+
+        document.getElementById("variation").value = vars[0];
+        document.getElementById("variationPrice").value = product_unit_price;
+
+    }
+
+
+
+    function dateCheck(sdate,edate,cdate) {
+
+        var start_date,end_date,current_date;
+        start_date = Date.parse(sdate);
+        end_date = Date.parse(edate);
+        current_date = Date.parse(cdate);
+
+        if((current_date <= end_date && current_date >= start_date)) {
+            return true;
+        }
+        return false;
+    }
+
+
+    function getTodayDate(){
+
+        today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //As January is 0.
+        var yyyy = today.getFullYear;
+
+        var sp = "/";
+
+        if(dd<10) dd='0'+dd;
+        if(mm<10) mm='0'+mm;
+
+        return (mm+sp+dd+sp+yyyy);
+    }
+
+
+    function formatPrice(value, currency){
+
+        var formatter = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: currency,
+        });
+
+        var price = formatter.format(value);
+
+        return price;
+    }
+
+
+    function setProductAttribute(){
+
+        var attribute = document.getElementById("attributeSelect").options[document.getElementById("attributeSelect").selectedIndex].value;
+
+        console.log(attribute);
+
+        document.getElementById("atrribute").value = attribute;
+    }
+
+</script>
