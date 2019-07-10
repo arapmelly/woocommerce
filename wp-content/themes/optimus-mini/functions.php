b0f1ef8e7eb28a18f7d3e7974c39592c4114dadf
Jacob Chumo <002028@scangroup.local>
2019-07-10 10:23:02 +0300

    fixed phone number on order

--- wp-content/themes/optimus-mini/functions.php
+++ wp-content/themes/optimus-mini/functions.php
@@ -138,7 +138,7 @@ function custom_override_checkout_fields($fields) {
 	unset($fields['billing']['billing_postcode']);
 	unset($fields['billing']['billing_country']);
 	unset($fields['billing']['billing_state']);
-	unset($fields['billing']['billing_phone']);
+	//unset($fields['billing']['billing_phone']);
 	unset($fields['order']['order_comments']);
 	//unset($fields['billing']['billing_email']);
 	//unset($fields['account']['account_username']);
