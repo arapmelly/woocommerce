b8700d817ddf2a5d4eb5d26e9d4c64cfc3a2bce8
Kelvin Mwangi <kelvinmwangi@Kelvinmwangi.local>
2019-08-27 12:14:19 +0300

    adding verified badge

--- wp-content/themes/optimus-mini/footer.php
+++ wp-content/themes/optimus-mini/footer.php
@@ -3,9 +3,16 @@
     <div class="inner-wrapper">
         <h3>CONTACT US</h3>
         <ul>
-	        <?php if (get_option('blogprimaryphonenumber')) {?><li><span>Phone:</span> <a href="tel:<?php echo get_option('blogprimaryphonenumber'); ?>"> <?php echo get_option('blogprimaryphonenumber'); ?></a></li><?php }?>
-	        <?php if (get_option('admin_email')) {?><li><span>Email:</span> <a href="mailto:<?php echo get_option('admin_email'); ?>"><?php echo get_option('admin_email'); ?></a></li><?php }?>
-            <?php if (get_option('blogprimaryaddress')) {?><li><span>Address:</span> <?php echo get_option('blogprimaryaddress'); ?></li><?php }?>
+            <?php if (get_option('blogprimaryphonenumber')) { ?>
+                <li><span>Phone:</span> <a
+                        href="tel:<?php echo get_option('blogprimaryphonenumber'); ?>"> <?php echo get_option('blogprimaryphonenumber'); ?></a>
+                </li><?php } ?>
+            <?php if (get_option('admin_email')) { ?>
+                <li><span>Email:</span> <a
+                        href="mailto:<?php echo get_option('admin_email'); ?>"><?php echo get_option('admin_email'); ?></a>
+                </li><?php } ?>
+            <?php if (get_option('blogprimaryaddress')) { ?>
+                <li><span>Address:</span> <?php echo get_option('blogprimaryaddress'); ?></li><?php } ?>
         </ul>
 
         <div class="copyright">
@@ -16,6 +23,13 @@
                 <li><a href="#">Terms of Use</a></li>
 
             </ul>
+
+<!--            <div class="seal">-->
+                <span id="siteseal">
+                    <script async type="text/javascript"
+                                            src="https://seal.godaddy.com/getSeal?sealID=HhUTlNug1eynlbSH1Cx7pT6wGCkzW5K8K9x2Q6OdAoaxEbpcBUWDTASoTLpg"></script>
+                </span>
+<!--            </div>-->
         </div>
 
     </div>
